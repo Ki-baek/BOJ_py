a5b8770c470a2a8ea87c3edab1e5043147b7edcc
Ki-baek <75228498+Ki-baek@users.noreply.github.com>
2025-02-26 00:00:22 +0900

    Create ❌1260 DFS와 BFS.py

---
+++ 1000/❌1260 DFS와 BFS.py
@@ -0,0 +1,47 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+n, m, v = map(int,input().split())
+
+links = {i : [] for i in range(1, n + 1)}
+
+def dfs(graph, start):
+    visited = deque()
+    lst = deque()
+    
+    lst.append(start)
+    
+    while lst:
+        n = lst.popleft()
+        
+        if n not in visited:
+            visited.append(n)
+            lst.clear()
+            lst.extend(sorted(graph[n]))
+    
+    return visited
+
+def bfs(graph, start):
+    visited = deque()
+    lst = deque()
+    
+    lst.append(start)
+    
+    while lst:
+        n = lst.popleft()
+        
+        if n not in visited:
+            visited.append(n)
+            lst.extend(sorted(graph[n]))
+    
+    return visited
+    
+for i in range(m):
+    a, b = map(int, input().split())
+    links[a].append(b)
+    links[b].append(a)
+
+print(*(dfs(links, v)))
+print(*(bfs(links, v)))
