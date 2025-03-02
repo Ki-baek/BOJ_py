07b6e28b686026c2007bddd7fb470e88a67d9616
Ki-baek <75228498+Ki-baek@users.noreply.github.com>
2025-03-02 14:32:59 +0000

    commit

--- 10000/18870 좌표압축.py
+++ 10000/18870 좌표압축.py
@@ -1,15 +1,15 @@
 import sys
-input = sys.stdin.readline()
+input = sys.stdin.readline
 
-n = int(input())
+n = int(input().strip())
 
-x = [int(input().split()) for i in range(n)]
+lst = list(map(int, input().strip().split()))
 
-x_prime = [0] * (n + 1)
+lst_set = sorted(list(set(lst)))
+dic = {}
 
-for i in range(n):
-    for j in range(n):
-        if x[i] > x[j]:
-            x_prime[i] += 1
+for i in range(len(lst_set)):
+    dic[lst_set[i]] = i #dic key == lst elements, dic value == "X prime(in question)"
 
-print(*x_prime)
+for i in lst:
+    print(dic[i], end = ' ') # getting "X primes" by calling lst elements as dic key
