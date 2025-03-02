01723bb93befaf21a733c53e3f40056e62fd397c
Ki-baek <75228498+Ki-baek@users.noreply.github.com>
2025-03-02 16:00:45 +0000

    commit

--- 1000/1654 랜선 자르기.py
+++ 1000/1654 랜선 자르기.py
@@ -1,5 +1,4 @@
 import sys
-sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 
 k, n = map(int, input().split())
@@ -9,6 +8,9 @@ lans_i_own = [int(input().strip()) for i in range(k)]
 longest_lan = max(lans_i_own)
 
 def searching(low, high):
+    if low > high:
+        return#이거 유무가 recursion error 생기게 할줄 몰랐지..
+    
     global n
     global max_lan
 
@@ -25,5 +27,5 @@ def searching(low, high):
     else:
         searching(low, mid - 1)
 
-searching(0, longest_lan)
+searching(1, longest_lan)#ZeroDivisionError, low 초기값 0이 아니라 1
 print(max_lan)
