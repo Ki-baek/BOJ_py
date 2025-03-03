d9726ab9476e21b51168e02beb322ef5bcb46163
Ki-baek <75228498+Ki-baek@users.noreply.github.com>
2025-03-03 13:28:19 +0000

    commit

--- 2000/2630 색종이 만들기.py
+++ 2000/2630 색종이 만들기.py
@@ -1,7 +1,14 @@
+# 1018 유사문제-?
 import sys
 input = sys.stdin.readline
 
+def div_paper()
+
+
 n = int(input().strip())
 
-paper = [list(map(int, input().split()) for i in range(n))]
+paper = []
+
+for i in range(n):
+    paper.append(list(map(int, input().split())))
 
