fe1d039ed496968b2851161b722eccc8a2b88e73
Ki-baek <75228498+Ki-baek@users.noreply.github.com>
2025-03-05 13:34:33 +0000

    commit

--- 2000/2941 크로아티아 알파벳.py
+++ 2000/2941 크로아티아 알파벳.py
@@ -1,11 +1,13 @@
 import sys
 input = sys.stdin.readline
 
-
 words = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
 
+
 string = input().strip()
-length = len(string)
+
 for i in words:
     if i in string:
-        string.count(i)
+        string = string.replace(i, 'a')
+
+print(len(string))
