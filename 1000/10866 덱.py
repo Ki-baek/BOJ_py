50ce4d971e5eae93c140050efb832ed97c581a77
Yub <75228498+Ki-baek@users.noreply.github.com>
2025-03-07 14:44:36 +0000

    commit

--- 1000/10866 덱.py
+++ 1000/10866 덱.py
@@ -1,6 +1,54 @@
+import sys
+input = sys.stdin.readline
+
 from collections import deque
 
-n = int(input())
+n = int(input().strip())
 
 lst = deque()
 
+for i in range(n):
+    command = input().split()
+
+    if command[0] == 'push_back':
+        lst.append(command[1])
+
+    elif command[0] == 'push_front':
+        lst.appendleft(command[1])
+
+    elif command[0] == 'pop_front':
+        if len(lst) == 0:
+            print(-1)
+        else:
+            print(lst.popleft())
+
+    elif command[0] == 'pop_back':
+        if len(lst) == 0:
+            print(-1)
+        else:
+            print(lst.pop())
+
+    elif command[0] == 'size':
+        print(len(lst))
+
+    elif command[0] == 'empty':
+        if len(lst) == 0:
+            print(1)
+        else:
+            print(0)
+
+    elif command[0] == 'front':
+        if len(lst) == 0:
+            print(-1)
+        else:
+            k = lst.popleft()
+            print(k)
+            lst.appendleft(k)
+
+    elif command[0] == 'back':
+        if len(lst) == 0:
+            print(-1)
+        else:
+            k = lst.pop()
+            print(k)
+            lst.append(k)
