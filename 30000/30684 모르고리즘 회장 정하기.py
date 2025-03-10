19520aa1bfafef586cdb93b6c7b3b1efc3263254
Yub <75228498+Ki-baek@users.noreply.github.com>
2025-03-10 14:39:42 +0000

    commit

--- 30000/30684 모르고리즘 회장 정하기.py
+++ 30000/30684 모르고리즘 회장 정하기.py
@@ -6,11 +6,15 @@ n = int(input().strip())
 lst_people = []
 
 for i in range(n):
-    name = list(input().strip())
-    if len(name) == 3:
+    name = input().strip()
+    if len(list(name)) == 3:
         lst_people.append(name)
 
 length = len(lst_people)
 candidate = lst_people[0]
+
 for i in range(1, length):
-    print
+    if candidate > lst_people[i]:
+        candidate = lst_people[i]
+
+print(candidate)
