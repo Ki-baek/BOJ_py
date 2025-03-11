d09c3540066792a855a000b15410be373ef38ce0
Yub <75228498+Ki-baek@users.noreply.github.com>
2025-03-11 14:24:06 +0000

    commit

--- 2000/2630 색종이 만들기.py
+++ 2000/2630 색종이 만들기.py
@@ -1,14 +1,34 @@
-# 1018 유사문제-?
 import sys
 input = sys.stdin.readline
 
-def div_paper()
+def div_paper(x, y, k):
+    color = paper[y][x]
+    for i in range(y, y + k):
+        for j in range(x, x + k):
+            if color != paper[i][j]:
+                l = k // 2
+                div_paper(x, y + l, l)
+                div_paper(x + l, y, l)
+                div_paper(x, y, l)
+                div_paper(x + l, y + l, l)
+                return
+    
+    if color == 0:
+        result[0] += 1
+    else:
+        result[1] += 1
+
 
 
 n = int(input().strip())
-
 paper = []
 
 for i in range(n):
     paper.append(list(map(int, input().split())))
 
+
+result = [0] * 2
+
+div_paper(0, 0, n)
+
+print(result[0], '\n',result[1], sep = '')
