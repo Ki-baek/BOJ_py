2b8a23a82c0b0e77fed91ecbfcd68796031463e0
Yub <75228498+Ki-baek@users.noreply.github.com>
2025-03-13 14:18:04 +0000

    commit

--- 1000/1697 숨바꼭질.py
+++ 1000/1697 숨바꼭질.py
@@ -1,1 +1,26 @@
+from collections import deque
 
+def search():
+    queue = deque()
+
+    queue.append(n)
+
+    while queue:
+        a = queue.popleft()
+
+        if a == k:
+            print(visited[a])
+            break
+        
+        for i in (a - 1, a + 1, a * 2):
+            if 0 <= i <= 100000 and not visited[i]:
+                visited[i] = visited[a] + 1
+                queue.append(i) 
+
+n, k = map(int, input().split())
+
+limit = 100000
+
+visited = [0] * (limit + 1)
+
+search()
