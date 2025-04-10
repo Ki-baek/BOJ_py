3eb4f05d5f124f07ab0b0945777c69b6d35301ea
Wooyeop Chung <75228498+Ki-baek@users.noreply.github.com>
2025-04-10 14:36:55 +0000

    commit

--- 30000/30804 과일 탕후루.py
+++ 30000/30804 과일 탕후루.py
@@ -4,25 +4,35 @@
 # 투포인터라는 개념이 나한테는 생소한데 문제가 참 많이나와있다
 # https://dongdongfather.tistory.com/69 << defaultdict 설명 유?사 딕셔너리 란다#
 # https://m.blog.naver.com/kks227/220795165570 << 투 포인터 관련글
+import sys
+input = sys.stdin.readline
+
 n = int(input())
 
 huru = list(map(int, input().split()))
 
-start, end = 0, len(huru) - 1
+left = 0
+cnt = {}
+maxlen = 0
 
-while 1:
-    if len(set(huru[start:end + 1])) > 2:
-        if start <= end - 1:
-            end -= 1
-        if start + 1 <= end:
-            start += 1
+for right in range(n):
+    now = huru[right]
+
+    if now in cnt:
+        cnt[now] += 1
     else:
-        if len(set(huru[start - 1: end + 1])) <= 2 and start > 0:
-            start -= 1
-        elif len(set(huru[start:end])) <= 2:
-            end += 1
-            if end >= len(huru):
-                end -= 1
-        break
+        cnt[now] = 1
 
-print(end - start + 1)
+    while len(cnt) > 2:
+        remove = huru[left]
+
+        cnt[remove] -= 1
+
+        if cnt[remove] == 0:
+            del cnt[remove]
+        
+        left += 1
+
+    maxlen = max(maxlen, right - left + 1)
+
+print(maxlen)
