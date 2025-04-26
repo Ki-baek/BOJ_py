5a48d3586e5e56f545e7d600caa3bbeff09c143d
Wooyeop Chung <75228498+Ki-baek@users.noreply.github.com>
2025-04-26 14:45:37 +0000

    commit

--- 6000/6064 카잉 달력.py
+++ 6000/6064 카잉 달력.py
@@ -1,1 +1,23 @@
+import sys
+input = sys.stdin.readline
 
+T = int(input().strip())
+
+for i in range(T):
+    i, j = 1, 1
+    cnt = 1
+    M, N, x, y = map(int, input().strip().split())
+
+    while (i != x and j != y) or (i != M and j != N):
+        if i < M:
+            i += 1
+        else:
+            i = 1
+
+        if j < N:
+            j += 1
+        else:
+            j = 1
+        cnt += 1
+    
+    print(cnt)
