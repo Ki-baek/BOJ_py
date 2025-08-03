48f17b2dc9f9d23c721537bb0c01acc6fa9b9850
woora03@naver.com <woora03@naver.com>
2025-08-03 11:49:31 +0900

    commit

--- 5000/5622 다이얼.py
+++ 5000/5622 다이얼.py
@@ -1,10 +1,11 @@
-dial = list(input())
+dial = ['ABC', 'DEF','GHI', 'JKL', 'MNO', 'PQRS', 'TUV', 'WXYZ']
 
-'''phone = {3 : ['A', 'B', 'C'], 4 : ['D', 'E', 'F'], 5 : ['G', 'H', 'I'], 6 : ['J', 'K', 'L'], 
-         7 : ['M','N','O'], 8 : ['P', 'Q', 'R', 'S'], 9 : ['T', 'U', 'V'], 10 : ['W', 'X', 'Y', 'Z']}'''
+string = list(input())
+result = 0
 
-phone_target = {v : k for k, v in phone.items()}
+for j in range(len(string)):
+  for i in dial:
+    if string[j] in i:
+      result += dial.index(i) + 3
 
-while dial:
-  letter = dial.pop()
-  phone_target.get(letter)
+print(result)
