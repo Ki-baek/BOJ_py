b7a917d3c99493f6e259417f2c6ad37f55fa104f
woora03@naver.com <woora03@naver.com>
2026-03-16 14:20:14 +0900

    commit

--- 10000/11053 가장 긴 증가하는 부분 수열.py
+++ 10000/11053 가장 긴 증가하는 부분 수열.py
@@ -2,18 +2,24 @@ n = int(input())
 
 num_lst = list(map(int, input().split()))
 
-max_len = [1]
-max_num = [num_lst[0]]
+result = [num_lst[0]]
 
 for i in range(1, n):
-    for j in range(len(max_num)):
-        if max_num[j] > num_lst[i]:
-            max_num.append(num_lst[i])
-            max_len.append(1)
-        elif max_num[j] < num_lst[i]:
-            max_num[j] = num_lst[i]
-            max_len[j] += 1
-        else:
-            continue
+    
+    if num_lst[i] <= result[-1]:
+        for j in range(len(result)):
+            if result[j] >= num_lst[i]:
+                result[j] = num_lst[i]
+                break
+            else:
+                continue
+    else:
+            result.append(num_lst[i])
+       
+    
+print(len(result))
 
-print(max(max_len))
+
+# 진짜 존나 어렵네요
+# 이진 탐색이 단순히 중간지점 찍고 어쩌구 하는게 아니고
+# 위에 문제처럼 길이만 요구하는 문제에서는 실제 배열의 모습 같은 것은 별로 중요하지 않다는 생각도 해야함
