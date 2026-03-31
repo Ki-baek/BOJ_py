3adeb0d86f8bd03a967a0b09e10b23e58ab6aa3c
Ki-baek <woora03@naver.com>
2026-03-31 11:03:15 +0900

    commit 34691

---
+++ 30000/34691 대전과학고등학교를 사랑하십니까.py
@@ -0,0 +1,7 @@
+while True:
+  string = input()
+  
+  if string ==  'end': break
+  elif string == 'animal': print('Panthera tigris')
+  elif string == 'flower': print('Forsythia koreana')
+  elif string == 'tree': print('Pinus densiflora')
